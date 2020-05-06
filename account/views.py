78030d9f5c37c95534234f12556efc0731c7e79d
bungouk6829@gmail.com <bungouk6829@gmail.com>
2020-05-06 18:13:10 +0900

    westagram ver 0.4

--- account/views.py
+++ account/views.py
@@ -8,13 +8,16 @@ class AccountView(View):
 	def post(self,request):
 		data = json.loads(request.body)
 		try:
-			Account.objects.create(
-				name = data['name'],
-				phone_number = data['phone_number'],
-				email = data['email'],
-				password = data['password']
-			)
-			return HttpResponse(status=200)
+			if Account.objects.filter(email=data['email']):
+				return JsonResponse({'message':'EMAIL_ALREADY_EXIST'}, status=400)
+			else:
+				Account.objects.create(
+					name = data['name'],
+					phone_number = data['phone_number'],
+					email = data['email'],
+					password = data['password']
+				)
+				return HttpResponse(status=200)
 
 		except KeyError:
 			return JsonResponse({'message':'INVALIDE_KEY'}, status=400)
@@ -23,7 +26,7 @@ class AccountView(View):
 		try :
 			return JsonResponse({'data':account_data}, status=200)
 		except Account.DoesNotExist:
-			return JsonResponse({'message':'Account_DOES_NOT_EXIST'}, status=400)
+			return JsonResponse({'message':'ACCOUNT_DOES_NOT_EXIST'}, status=400)
 
 class SignUpView(View):
 	
